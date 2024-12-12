32713faaf8dc42ab3e08ce9e6e3c6e2b8e13682c
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2024-12-12 09:24:39 -0300

    Todos os desafios do curso: resoluções

---
+++ all_coding_challenges/fundamentals_pt1/coding_challenge_01.js
@@ -0,0 +1,11 @@
+const weight_Mark = 78;  // uso de const porque nessa situação especifica não ocorrerá mudanças nos valores
+const weight_John = 92;  // 
+const height_Mark = 1.69;  //
+const height_John = 1.95;  //
+
+const BMI_Mark = weight_Mark / (height_Mark ** 2);
+const BMI_John = weight_John / (height_John ** 2);
+
+const markHigherBMI = BMI_Mark > BMI_John;
+
+console.log(BMI_John.toFixed(2), BMI_Mark.toFixed(2));
