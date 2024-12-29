30f9aef6672d535ca7fc20a9bb5a8925fbbc4e97
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2024-12-29 14:33:03 -0300

    Activating strict mode in section 2

---
+++ 02_Fundamentals_pt2/Activating_Strict_Mode/script.js
@@ -0,0 +1,11 @@
+'use strict';  // deve ser o primeiro script do código inteiro
+// strict fala onde erramos
+
+let hasDriversLiscense = false;
+const passTest = true;
+
+if (passTest) hasDriverLiscense = true; // ele avisará que está escrito de maneira errada
+if (hasDriversLiscense) console.log(`I can drive!`);
+
+// const interface = 'Audio';  // mostra que a palavra 'interface' é reservada para js
+const private = 534;  // também reserva a palavra
