d34e69536cf824d1111bae037b19896ff5330738
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2025-02-20 21:44:06 -0300

    Problema 07: Comparação de MBIs

---
+++ all_coding_challenges/fundamentals_pt2/coding_challenge_07.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function (mass, height) {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function (mass, height) {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+
+mark.calcBMI(); //chamando a função 
+john.calcBMI();
+
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is hight than ${john.fullName}'s BMI (${john.bmi})`);
+} else if (mark.bmi < john.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is hight than ${mark.fullName}'s BMI (${mark.bmi})`);
+} else {
+    console.log(`Same values!`);
+}
