7bf1c7220b838fddfb44f52668412ffd9bfde3c8
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2025-03-05 20:28:20 -0300

    Problema 08: Cálculo de gorjetas com loop for

---
+++ all_coding_challenges/fundamentals_pt2/coding_challenge_08.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+for (let i = 0; i < bills.length; i++) {
+    tips[i] = calcTip(bills[i]);
+    totals[i] = bills[i] + tips[i];
+}
+
+console.log(tips);
+console.log();
+console.log(totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length
+}
+
+console.log(calcAverage([2, 5, 3]));
